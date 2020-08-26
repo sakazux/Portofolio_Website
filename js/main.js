b90ad49fc71bb1e502607445d0d9bda8cd2c2b54
sakazux <61787566+sakazux@users.noreply.github.com>
2020-08-26 10:54:36 +0545

    smooth scroling achived

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
 
+    // scroll nav bar
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
 
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 70
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
     // sticky navigation menu
 
     let nav_offset_top = $('.header_area').height() + 50;
